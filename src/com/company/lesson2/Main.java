6cf7b164b462522cb9a1342d7c1d5926b6ca1b48
Leonid Nikiforov <nikiforov.leonid@rt.ru>
2021-12-20 21:56:55 +0300

    improved

--- src/com/company/lesson2/Main.java
+++ src/com/company/lesson2/Main.java
@@ -1,7 +1,5 @@
 package com.company.lesson2;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         // Корректный массив
@@ -14,29 +12,10 @@ public class Main {
         String[][][] examples = {rightArr, wrongSizeArr, wrongDataArr};
         for (int i = 0; i < examples.length; i++) {
             try {
-                printSumOfArray(examples[i]);
-            } catch (MyArraySizeException | MyArrayDataException e) {
+                SumOfArray.print(examples[i]);
+            } catch (SumOfArray.MyArraySizeException | SumOfArray.MyArrayDataException e) {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    public static void printSumOfArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
-        if (arr.length != 4) throw new MyArraySizeException(arr);
-
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].length != 4) throw new MyArraySizeException(arr);
-
-            for (int j = 0; j < arr[i].length; j++) {
-                try {
-                    sum = sum + Integer.parseInt(arr[i][j]);
-                } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(arr, i, j);
-                }
-            }
-        }
-
-        System.out.println("Array " + Arrays.deepToString(arr) + " is ok. Sum = " + sum);
     }
 }
--- src/com/company/lesson2/MyArrayDataException.java
+++
@@ -1,9 +0,0 @@
-package com.company.lesson2;
-
-import java.util.Arrays;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String[][] arr, int i, int j) {
-        super("Array " + Arrays.deepToString(arr) + " is wrong. Error in cell [" + i + "][" + j + "]");
-    }
-}
